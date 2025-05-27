84298bff6bb59bd7aaa1539fe414423a6b856fea
Prateek Chaudhary <prateekchaudhary7805@gmail.com>
2025-05-27 11:09:46 +0530

    feat: initial commit with semantic-release setup

--- release.config.js
+++ release.config.js
@@ -1,14 +1,13 @@
-// release.config.js
 module.exports = {
-    branches: ['main'],
-    REPO_URL: ['https://github.com/chaudhary-prateek/lang-api.git']
+    repositoryUrl: "https://github.com/chaudhary-prateek/lang-api", // ✅ your actual repo URL
+    branches: ["main"],
     plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/changelog',
-      '@semantic-release/npm',
-      '@semantic-release/github',
-      '@semantic-release/git'
+      "@semantic-release/commit-analyzer",
+      "@semantic-release/release-notes-generator",
+      "@semantic-release/changelog",
+      "@semantic-release/npm",
+      "@semantic-release/github",
+      "@semantic-release/git"
     ]
   };
   
