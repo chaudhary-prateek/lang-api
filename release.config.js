66ec8c14441289c8026d28c033945e51027c3459
Prateek Chaudhary <prateekchaudhary7805@gmail.com>
2025-05-27 17:46:13 +0530

    feat: add login form

--- release.config.js
+++ release.config.js
@@ -3,9 +3,11 @@ module.exports = {
     repositoryUrl: "https://github.com/chaudhary-prateek/lang-api",
     branches: ["main"],
     plugins: [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        "@semantic-release/changelog",
-        "@semantic-release/git"
-      ]
+      "@semantic-release/commit-analyzer",
+      "@semantic-release/release-notes-generator",
+      "@semantic-release/changelog",
+      "@semantic-release/npm",
+      "@semantic-release/github",
+      "@semantic-release/git"
+    ]
   };
