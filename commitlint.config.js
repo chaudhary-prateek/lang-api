18914c826af39d227461342ab2ea640a686294b2
Prateek Chaudhary <prateekchaudhary7805@gmail.com>
2025-05-28 12:33:04 +0530

    feat: initial commit with semantic-release setup

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,12 @@
 // commitlint.config.js
-module.exports = {
-    extends: ['@commitlint/config-conventional'],
+//module.exports = {
+//    extends: ['@commitlint/config-conventional'],
+//  };
+//
+//  
+  module.exports = {
+    rules: {
+      'body-max-line-length': [0, 'always', 100], // Disable the rule (0 = off)
+    },
   };
   
