97f1a8d33cbcfcb91d67665debc34e136a8f719f
Prateek Chaudhary <prateekchaudhary7805@gmail.com>
2025-05-28 12:23:36 +0530

    feat: initial commit with semantic-release setup

--- release.config.js
+++ release.config.js
@@ -1,13 +1,31 @@
-// release.config.js
-module.exports = {
-    repositoryUrl: "https://github.com/chaudhary-prateek/lang-api",
-    branches: ["main"],
+//// release.config.js
+//module.exports = {
+//    repositoryUrl: "https://github.com/chaudhary-prateek/lang-api",
+//    branches: ["main"],
+//    plugins: [
+//      "@semantic-release/commit-analyzer",
+//      "@semantic-release/release-notes-generator",
+//      "@semantic-release/changelog",
+//      "@semantic-release/npm",
+//      "@semantic-release/github",
+//      "@semantic-release/git"
+//    ]
+//  };
+
+  module.exports = {
+    branches: ['main'],
     plugins: [
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/changelog",
-      "@semantic-release/npm",
-      "@semantic-release/github",
-      "@semantic-release/git"
-    ]
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/changelog',
+      [
+        '@semantic-release/git',
+        {
+          assets: ['CHANGELOG.md', 'package.json'],
+          message: 'chore(release): ${nextRelease.version} [skip ci]',
+        },
+      ],
+      '@semantic-release/github',
+    ],
   };
+  
